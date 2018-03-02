94022f6821fa4e80cef6d27047f66b67ede3c54b
Mrityunjay Saxena <mrityunjay.saxena.met14@iitbhu.ac.in>
2018-03-02 23:37:40 +0530

    volume, backlight, brightness etc added to openbox

--- polybar/mpris.sh
+++ polybar/mpris.sh
@@ -2,7 +2,8 @@
 
 # Specifying the icon(s) in the script
 # This allows us to change its appearance conditionally
-icon=""
+icon_play=""
+icon_pause=""
 
 player_status=$(playerctl status 2> /dev/null)
 if [[ $? -eq 0 ]]; then
@@ -11,9 +12,9 @@ fi
 
 # Foreground color formatting tags are optional
 if [[ $player_status = "Playing" ]]; then
-    echo "%{F$(xrdb -query | grep '*color2'| awk '{print $NF}')}$icon $metadata"       # Orange when playing
+    echo "%{F$(xrdb -query | grep '*color2'| awk '{print $NF}')}$icon_play $metadata"       # Orange when playing
 elif [[ $player_status = "Paused" ]]; then
-    echo "%{F$(xrdb -query | grep '*color4'| awk '{print $NF}')}$icon $metadata"       # Greyed out info when paused
+    echo "%{F$(xrdb -query | grep '*color4'| awk '{print $NF}')}$icon_pause $metadata"       # Greyed out info when paused
 else
     echo "%{F$(xrdb -query | grep '*color7'| awk '{print $NF}')}$icon"                 # Greyed out icon when stopped
 fi
